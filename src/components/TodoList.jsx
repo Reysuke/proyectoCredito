ebcecb3745888bbc700d7c71fd59de1a943d958d
blennis <blennis@correouninorte.edu.co>
2021-11-23 15:20:11 -0500

    first commit

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export function TodoList(){
+    return(
+        <ul>
+            {listas.map((lista)=>(
+                <li></li>
+            ))}
+        </ul>
+
+    );
+}
